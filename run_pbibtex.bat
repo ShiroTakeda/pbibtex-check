bf732a66bb2f1d837bfa744c2efa9d2631ec5a3a
ShiroTakeda <shiro.takeda@gmail.com>
2021-05-12 13:15:53 +0900

    修正。

--- run_pbibtex.bat
+++ run_pbibtex.bat
@@ -1,10 +1,10 @@
 
 
-call sub_run_pbibtex g 2021 lualatex upbibtex test_upbibtex
-call sub_run_pbibtex g 2020 lualatex upbibtex test_upbibtex
-call sub_run_pbibtex g 2019 lualatex upbibtex test_upbibtex
+start sub_run_pbibtex g 2021 uplatex upbibtex test_upbibtex
+start sub_run_pbibtex g 2020 uplatex upbibtex test_upbibtex
+start sub_run_pbibtex g 2019 uplatex upbibtex test_upbibtex
 
-call sub_run_pbibtex g 2021 lualatex pbibtex test_pbibtex
-call sub_run_pbibtex g 2020 lualatex pbibtex test_pbibtex
-call sub_run_pbibtex g 2019 lualatex pbibtex test_pbibtex
+start sub_run_pbibtex g 2021 platex pbibtex test_pbibtex
+start sub_run_pbibtex g 2020 platex pbibtex test_pbibtex
+start sub_run_pbibtex g 2019 platex pbibtex test_pbibtex
 
--- sub_run_pbibtex.bat
+++ sub_run_pbibtex.bat
@@ -1,11 +1,9 @@
 
-copy %5.tex %5_%2.tex
-
 @title 1st %3 on %5
 call %1:\texlive\%2\bin\win32\%3 %5_%2.tex
 
 @title upbibtex on %5
-call %1:\texlive\%2\bin\win32\%4 %5_%2
+call %1:\texlive\%2\bin\win32\%4 %5_%2.aux
 
 @title 2nd %3 on %5
 call %1:\texlive\%2\bin\win32\%3 %5_%2.tex
@@ -13,7 +11,12 @@ call %1:\texlive\%2\bin\win32\%3 %5_%2.tex
 @title 3rd %3 on %5
 call %1:\texlive\%2\bin\win32\%3 %5_%2.tex
 
+@title dvipdfmx on %5
+call %1:\texlive\%2\bin\win32\dvipdfmx %5_%2.dvi
+
 del %5_%2.log
 del %5_%2.aux
 del %5_%2.blg
 del %5_%2.bbl
+
+exit
